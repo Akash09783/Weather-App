a7824cc3eb3172341e0d124fa8a9f1f27c34bba5
Akash <kumawat.aakash515@gmail.com>
2024-01-13 15:24:44 +0530

    Weather APi added

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
- 
+ const apiKey = "f267ae893b31f3de434013fc0e11aae7";
+ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metrics&q=bangalore";
+
+ async function checkWeather(){
+    const response = await fetch(apiUrl + `&appid=${apiKey}`)
+    var data = await response.json();
+    console.log(data);
+    document.querySelector(".city").innerHTML = data
+ }
+checkWeather();
